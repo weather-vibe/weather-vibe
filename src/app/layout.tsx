a075f700ceeacac7f9564d45f59bb0394e725605
haaymonster <imhayleylee@gmail.com>
2024-04-23 23:02:47 +0900

    delete weather path and move to default path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SearchBar from "@/components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +12,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className="flex flex-col w-full h-screen">{children}</body>
+      <body className="flex flex-col w-full h-screen">
+        <SearchBar></SearchBar>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 import Image from "next/image";
+import { Suspense } from "react";
+import WeatherInfo from "../components/WeatherInfo";
+import YtPlayer from "../components/YtPlayer";
 
-export default function Home() {
-  return <main></main>;
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: { lat?: string; lon?: string };
+}) {
+  return (
+    <div>
+      <Suspense fallback={<div>!!!</div>}>
+        <WeatherInfo searchParams={searchParams}></WeatherInfo>
+      </Suspense>
+      <YtPlayer></YtPlayer>
+    </div>
+  );
 }
+
+// export default function Home() {
+//   return <main></main>;
+// }
