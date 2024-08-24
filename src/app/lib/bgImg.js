cb0633161885e908b7e452213972b4376ae02e0d
haaymonster <imhayleylee@gmail.com>
2024-08-25 00:09:33 +0900

    이미지 삭제

--- src/app/lib/bgImg.js
+++ src/app/lib/bgImg.js
@@ -7,6 +7,7 @@ import nightBeach from "./bg/nightBeach.gif";
 import peoples from "./bg/peoples.gif";
 import sunsetBeach from "./bg/sunsetBeach.gif";
 import train from "./bg/train.gif";
+import cooking from "./bg/cooking.gif";
 
 const images = [
   car,
@@ -18,19 +19,7 @@ const images = [
   nightBeach,
   peoples,
   sunsetBeach,
-  "https://i.gifer.com/6OmH.gif",
-  "https://i.gifer.com/6vIk.gif",
-  "https://i.gifer.com/PPy.gif",
-  "https://i.gifer.com/380e.gif",
-  "https://i.gifer.com/NWi5.gif",
-  "https://i.gifer.com/2ruN.gif",
-  "https://i.gifer.com/TTOl.gif",
-  "https://i.gifer.com/YmYu.gif",
-  "https://i.gifer.com/8qG.gif",
-  "https://i.gifer.com/Cay5.gif",
-  "https://i.gifer.com/7jnE.gif",
-  "https://i.gifer.com/AI5S.gif",
-  "https://i.gifer.com/6Swh.gif",
+  cooking,
 ];
 
 export default images;
